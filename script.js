6c606d9dbcd3c13b6b1ae58dd7c91f250874058f
Nitesh Kumar Mishra <127732043+niteshmishra188@users.noreply.github.com>
2026-04-27 12:56:05 +0530

    Enhance tracking for button clicks with URL redirection

--- script.js
+++ script.js
@@ -365,12 +365,21 @@ scrollButtons.forEach(button => {
 // Initial scroll check
 handleScroll();
 //  Google tag (gtag.js)
-document.querySelectorAll('.track-btn').forEach(btn => {
-  btn.addEventListener('click', function () {
-    gtag('event', 'click', {
-      event_category: 'Outbound Link',
-      event_label: this.dataset.label,
-      value: 1
+document.addEventListener('DOMContentLoaded', function () {
+  document.querySelectorAll('.track-btn').forEach(btn => {
+    btn.addEventListener('click', function (e) {
+      e.preventDefault();
+
+      const url = this.href;
+
+      gtag('event', 'button_click', {
+        event_category: 'Outbound Link',
+        event_label: this.dataset.label,
+        transport_type: 'beacon',
+        event_callback: function () {
+          window.location.href = url;
+        }
+      });
     });
   });
 });
